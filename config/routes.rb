df17227c07249ece103da3499e1847ce81234f67
neffnet <neff@neffnet.net>
2015-05-27 18:24:57 -0500

    routes and views for new domain

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :domains, shallow: true
+  end
 
   get 'welcome/index'
   get 'welcome/about'
